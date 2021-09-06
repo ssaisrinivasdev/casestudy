29b530ef361ef7c20142994540fdd41a68109e96
S Sai Srinivas <ssaisrinivas616@gmail.com>
2021-09-06 18:30:24 +0530

    Updated Day 2 Assignments- Circle

---
+++ C# Assignment Codes/Day2/Circle/Circle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Circle
+{
+    class Circle
+    {
+        double radius;
+		public Circle(){}
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double GetRadius()
+        {
+            return radius;
+        }
+        public void SetRadius(double radius)
+        {
+            this.radius=radius;
+        }
+        public double calcDiameter() {
+            return radius * 2;
+        }
+        public double calcArea() {
+            return 3.14 * radius * radius;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Radius: ");
+            int radius = int.Parse(Console.ReadLine());
+            Circle c1 = new Circle();
+            c1.SetRadius(radius);
+            Console.WriteLine($"Radius: {c1.GetRadius()}");
+            Console.WriteLine(String.Format("Diameter: {0:0.##}", c1.calcDiameter()));
+            Console.WriteLine(String.Format("Area: {0:0.##}", c1.calcArea()));
+
+        }
+    }
+}
